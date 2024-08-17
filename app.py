11f0cf498d3b8ad3e93cba13008df178e5717702
NairyKumar <narendra.2020@vitstudent.ac.in>
2024-08-17 08:55:41 +0000

    flask app launch

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-print("kumar")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+  return "Hello,Naiy"
+
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', debug=True)
